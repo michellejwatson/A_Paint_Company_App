565f238419babc3d7e69199f956aa7263ff5452f
Michelle Watson <michellejwatson1@gmail.com>
2024-03-22 18:19:51 -0700

    Add Paint model class

--- django_backend/api/models.py
+++ django_backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Create Paint Model
+class Paint(models.Model):
+    STATUS_CHOICES = [
+        ('running_low', 'Running Low'),
+        ('available', 'Available'),
+        ('out_of_stock', 'Out of Stock'),
+    ]
+
+    colour = models.CharField(max_length=6)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='available')
+    inventory = models.PositiveIntegerField()
+
+    def _str_(self):
+        return self.title
