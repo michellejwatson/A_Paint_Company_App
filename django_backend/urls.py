d24b39982510e7adb448c31a8452b61078fe5f0b
Michelle Watson <michellejwatson1@gmail.com>
2024-03-24 17:50:55 -0700

    Add route to frontend in urls

--- django_backend/urls.py
+++ django_backend/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+
+    # Route all other paths to React frontend
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
